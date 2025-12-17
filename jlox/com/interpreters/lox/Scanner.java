689d1a3ea60acceeb27f31b07ec0c85963b639c4
charlie_tsai <charlie1123tsai@gmail.com>
2025-12-17 15:17:10 +0800

    Add (String, Number, Identifier, Multiline comment, Reserved word) Scanner

--- jlox/com/interpreters/lox/Scanner.java
+++ jlox/com/interpreters/lox/Scanner.java
@@ -12,6 +12,27 @@ class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("fun", FUN);
+        keywords.put("for", FOR);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -44,9 +65,48 @@ class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
-            // todo SLASH
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else if (match('*')) { 
+                    multiLineComment();
+                    // nestedMultiLineComment();
+                }else {
+                    addToken(SLASH);
+                }
+                break;
+
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+            
+            case '"': string(); break;
+            
             default:
-                Lox.error(line, "Unexpected charater.");
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected charater.");
+                }
                 break;
         }
     }
@@ -62,5 +122,104 @@ class Scanner {
     private void addToken(TokenType type, Object literal) {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd() || source.charAt(current) != expected) return false;
+        current++;
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (advance() == '\n') line++;
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        advance(); // '"'
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+        
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+            while (isDigit(peek())) advance();
+        }
+
+        double value = Double.parseDouble(source.substring(start, current));
+        addToken(NUMBER, value);
+    }
+
+    private char peekNext() {
+        if ((current + 1) >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               c == '_';
+    }
+
+    private boolean isAlphaDigit(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private void identifier() {
+        while (isAlphaDigit(peek())) advance();
+        String value = source.substring(start, current);
+        TokenType type = keywords.get(value);
+        if (type == null) type = IDENTIFIER;
+        addToken(type, value);
+    }
+
+    private void multiLineComment() {
+        while (!isAtEnd() && !(peek() == '*' && peekNext() == '/')) {
+            if (advance() == '\n') line++;
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Invalid multi-line comment.");
+            return;
+        }
+        advance();
+        advance();
+    }
+
+    private void nestedMultiLineComment() {
+        int nest = 1;
+        while (!isAtEnd() && nest != 0) {
+            if (peek() == '*' && peekNext() == '/') {
+                nest--;
+                advance();
+                advance();
+            } else if (peek() == '/' && peekNext() == '*') {
+                nest++;
+                advance();
+                advance();
+            } else if (advance() == '\n') {
+                line++;
+            }
+        }
+        if (nest != 0) {
+            Lox.error(line, "Invalid nested multi-line comment.");
+            return;
+        }
     }
 }
